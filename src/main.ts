47ed40719dc774e28bd2cd521000cccc0c2f5475
wolffanil2 <wolffanil@mail.ru>
2024-08-10 17:48:34 +0400

    fix vercel 2

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as morgan from 'morgan';
+// import * as morgan from 'morgan';
 import helmet from 'helmet';
 import * as ExpressMongoSanitize from 'express-mongo-sanitize';
 import { ValidationPipe } from '@nestjs/common';
@@ -8,9 +8,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-  }
+  // if (process.env.NODE_ENV === 'development') {
+  //   app.use(morgan('dev'));
+  // }
 
   app.use(helmet());
   app.use(ExpressMongoSanitize());
